d652b66a7002570a586889ce7d652fbfc611b59a
yc258 <3163006368@qq.com>
2026-06-04 23:16:55 +0800

    Enhance main function to greet user in Chinese

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main()
-{printf("hello,world!\n");
-return0;
+int main() {
+    char name[50];
+    printf("请输入你的名字：");
+    scanf("%s", name);
+    printf("你好，%s! 欢迎学习Git。\n", name);
+    return 0;
 }
